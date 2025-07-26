fa0dcf0fd3fc6069b84d1f57a47ed7e752a72186
Zerolight90 <ant7773@gmail.com>
2025-07-27 00:42:17 +0900

    기초 뼈대 작업중

--- src/main/java/Action/IndexAction.java
+++ src/main/java/Action/IndexAction.java
@@ -8,6 +8,6 @@ public class IndexAction implements Action {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
 
-        return "jsp/index.jsp";
+        return "index.jsp";
     }
 }
