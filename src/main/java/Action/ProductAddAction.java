1274c9a9f361a31d8837b7e036e7a2a1906b6d0e
mwj02 <moonwj5685@gmail.com>
2025-08-03 18:04:52 +0900

    관리자 상품정보화면 Modal 표현

---
+++ src/main/java/Action/ProductAddAction.java
@@ -0,0 +1,21 @@
+package Action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProductAddAction implements Action{
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        String category = request.getParameter("addCategory");
+        String productName = request.getParameter("addProductName");
+        String description = request.getParameter("addDescription");
+        String img = request.getParameter("addImg");
+        String price = request.getParameter("addPrice");
+        String stock = request.getParameter("addStock");
+        String status = request.getParameter("addStatus");
+
+
+        return "productList.jsp";
+    }
+}
