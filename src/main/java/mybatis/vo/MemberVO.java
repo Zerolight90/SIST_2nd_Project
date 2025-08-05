904f4fb860de5c393ad95827e2ba76ae130c7a18
wlsksekf <jinaniyagi@gmail.com>
2025-08-05 10:31:05 +0900

    1
    1

--- src/main/java/mybatis/vo/MemberVO.java
+++ src/main/java/mybatis/vo/MemberVO.java
@@ -1,86 +1,35 @@
 package mybatis.vo;
 
+import java.util.Date;
+
 public class MemberVO {
-    private String m_idx, m_name, m_id, m_pw, m_phone,
-            m_addr, reg_date, status, etc1, etc2;
+    private long userIdx;
+    private String id;
+    private String pw;
+    private Date birth;
+    private String name;
+    private String gender;
+    private String phone;
+    private String email;
+    private int totalPoints;
 
-    public String getM_idx() {
-        return m_idx;
-    }
-
-    public void setM_idx(String m_idx) {
-        this.m_idx = m_idx;
-    }
-
-    public String getM_name() {
-        return m_name;
-    }
-
-    public void setM_name(String m_name) {
-        this.m_name = m_name;
-    }
-
-    public String getM_id() {
-        return m_id;
-    }
-
-    public void setM_id(String m_id) {
-        this.m_id = m_id;
-    }
-
-    public String getM_pw() {
-        return m_pw;
-    }
-
-    public void setM_pw(String m_pw) {
-        this.m_pw = m_pw;
-    }
-
-    public String getM_phone() {
-        return m_phone;
-    }
-
-    public void setM_phone(String m_phone) {
-        this.m_phone = m_phone;
-    }
-
-    public String getM_addr() {
-        return m_addr;
-    }
-
-    public void setM_addr(String m_addr) {
-        this.m_addr = m_addr;
-    }
-
-    public String getReg_date() {
-        return reg_date;
-    }
-
-    public void setReg_date(String reg_date) {
-        this.reg_date = reg_date;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getEtc1() {
-        return etc1;
-    }
-
-    public void setEtc1(String etc1) {
-        this.etc1 = etc1;
-    }
-
-    public String getEtc2() {
-        return etc2;
-    }
-
-    public void setEtc2(String etc2) {
-        this.etc2 = etc2;
-    }
+    // Getter and Setter
+    public long getUserIdx() { return userIdx; }
+    public void setUserIdx(long userIdx) { this.userIdx = userIdx; }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getPw() { return pw; }
+    public void setPw(String pw) { this.pw = pw; }
+    public Date getBirth() { return birth; }
+    public void setBirth(Date birth) { this.birth = birth; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public int getTotalPoints() { return totalPoints; }
+    public void setTotalPoints(int totalPoints) { this.totalPoints = totalPoints; }
 }
