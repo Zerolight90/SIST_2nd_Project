1b592d7e6ba14af673e325c67eb6c9b1fbecbf5a
wlsksekf <jinaniyagi@gmail.com>
2025-08-26 12:29:43 +0900

    1
    1

--- src/main/java/Action/NonmemberAction.java
+++ src/main/java/Action/NonmemberAction.java
@@ -25,9 +25,18 @@ public class NonmemberAction implements Action {
         String pw    = request.getParameter("u_pw");
 
 
-        // 비회원 VO 생성
+        // 1. 전달받은 파라미터로 VO 생성
         NmemVO n = new NmemVO(null, name, email, null, phone, pw, birth);
 
+        // 2. DB에 동일한 정보의 비회원이 있는지 먼저 조회
+        NmemVO nmemvo = NmemDAO.findNmemByInfo(n);
+
+        if (nmemvo == null) {
+            // 3-1. 기존 정보가 없으면, 새로 등록
+            NmemDAO.registry(n);
+            // 방금 등록한 정보를 다시 조회해서 nmemvo에 저장
+            nmemvo = NmemDAO.findNmemByInfo(n);
+        }
         // DB에 저장
         int cnt = NmemDAO.registry(n);
 
